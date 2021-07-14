5755928fc8617ed445947f85acd9e792b07cb619
Ricardenho <andrade.comim@gmail.com>
2021-07-14 16:41:23 -0300

    configuração do BD no APP

---
+++ database.js
@@ -0,0 +1,10 @@
+var knex = require("knex")({
+    client:'mysql2',
+    connection:{
+        host:'127.0.0.1',
+        user:'root',
+        password:'nodejs',
+        database:'futebol'
+    }
+});
+module.exports = knex;
